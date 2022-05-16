07e4984300f7d962feeef4e7ed4bc58ea2d993bc
Alex4790354 <9720560@gmail.com>
2022-05-16 23:19:50 +0300

    Sec-16. Add Context path (part of URL) + change jar to war

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RESTful_Web_Services_Spring_Boot_MVC_JPA
 RESTful Web Services, Java, Spring Boot, Spring MVC and JPA
+
+
+
+
+Commands: 
+    mvn clean install
+    mvn spring-boot:run 
+    move or copy 
+    java -jar <file_name>
--- src/main/java/com.appsdeveloperblog.app.ws/service/impl/UserServiceImpl.java
+++ src/main/java/com.appsdeveloperblog.app.ws/service/impl/UserServiceImpl.java
@@ -129,7 +129,8 @@ public class UserServiceImpl implements UserService {
 	public List<UserDto> getUsers(int page, int limit) {
 		List<UserDto> returnValue = new ArrayList<>();
 
-		if(page>0) page = page-1;
+		if(page>0)
+			page = page - 1;
 
 		Pageable pageableRequest = PageRequest.of(page, limit);
 
