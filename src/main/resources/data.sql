29707332cc6d29bf1f5b92cb146d1d177196ae01
Alex4790354 <9720560@gmail.com>
2022-05-18 22:41:12 +0300

    Sec-18-20: add Relationships @OneToMany. Refactoring (wrong folder-name)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO users(user_id, first_name, last_name, email, encrypted_password, email_verification_token, email_verification_status) VALUES ('user007', 'Jhon', 'Smith', 'agent007@m6.com', 'encrepted', '123456', true);
+INSERT INTO users(user_id, first_name, last_name, email, encrypted_password, email_verification_token, email_verification_status) VALUES ('zRAPD8AGBu3D0E63bKsZqHpAXczBjb', 'Jhon', 'Smith', 'agent007@m6.com', '$2a$10$sxyPcu0alBd.jT.jHfPtRukvPgqkNXF4unm1MRXCN/0PtdHzRqypK', 'email_token', true);
 
+INSERT INTO addresses(address_id, city, country, street_name, postal_code, type, users_id) VALUES ('agent-007-address1', 'Toronto', 'Canada', '123 Yonge str.', 'A1B2C3', 'billing', 1);
+INSERT INTO addresses(address_id, city, country, street_name, postal_code, type, users_id) VALUES ('agent-007-address2', 'Markham', 'Canada', '456 Old Rd.', 'D3E4F4', 'shipment', 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-       id                   BIGSERIAL,
+       id                    BIGSERIAL,
        user_id               CHARACTER VARYING(255),
        first_name            CHARACTER VARYING(255),
        last_name             CHARACTER VARYING(255),
-       email                CHARACTER VARYING(255),
-       encrypted_password        CHARACTER VARYING(255),
+       email                 CHARACTER VARYING(255),
+       encrypted_password         CHARACTER VARYING(255),
        email_verification_token   CHARACTER VARYING(255),
        email_verification_status  boolean,
        CONSTRAINT users_pk      PRIMARY KEY (id)
@@ -13,7 +13,19 @@ CREATE TABLE users (
 
 ALTER TABLE users ADD CONSTRAINT users_email_unique UNIQUE(email);
 
+DROP TABLE IF EXISTS addresses CASCADE;
+CREATE TABLE addresses (
+       id                   BIGSERIAL,
+       address_id            CHARACTER VARYING(30),
+       city                  CHARACTER VARYING(15),
+       country               CHARACTER VARYING(15),
+       street_name           CHARACTER VARYING(100),
+       postal_code           CHARACTER VARYING(7),
+       type                  CHARACTER VARYING(10),
+       users_id              bigint,
+       CONSTRAINT addresses_pk PRIMARY KEY (id)
+);
 
---ALTER TABLE book ADD CONSTRAINT book_fk_01 FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE;
+ALTER TABLE addresses ADD CONSTRAINT addresses_fk_01 FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE;
 
 
